51e4319035ef41df4f664875d81cfd6b3188055b
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-06-17 23:32:57 +0800

    initiize

--- openvoice/se_extractor.py
+++ openvoice/se_extractor.py
@@ -19,7 +19,11 @@ model = None
 def split_audio_whisper(audio_path, audio_name, target_dir='processed'):
     global model
     if model is None:
-        model = WhisperModel(model_size, device="cuda", compute_type="float16")
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        if device == "cuda":
+            model = WhisperModel(model_size, device=device, compute_type="float16")
+        else:
+            model = WhisperModel(model_size, device=device)
     audio = AudioSegment.from_file(audio_path)
     max_len = len(audio)
 
